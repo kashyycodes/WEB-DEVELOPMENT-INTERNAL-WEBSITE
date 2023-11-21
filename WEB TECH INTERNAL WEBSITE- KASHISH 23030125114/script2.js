36b537f4a4c134f79d1cdf295375fdf082535531
kashyycodes <149258948+kashyycodes@users.noreply.github.com>
2023-11-21 11:16:25 +0530

    Add files via upload

---
+++ WEB TECH INTERNAL WEBSITE- KASHISH 23030125114/script2.js
@@ -0,0 +1,6 @@
+var menuIcon = document.querySelector(".menu-icon");
+var sideBar = document.querySelector(".sidebar");
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+}
